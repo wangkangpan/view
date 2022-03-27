cb228e020dbf92109c414b58e7b89815b84c0327
wangkangpan <1456225949@qq.com>
2022-03-27 20:35:07 +0800

    2022-3-27

--- user/src/main/resources/static/js/home.js
+++ user/src/main/resources/static/js/home.js
@@ -10,22 +10,27 @@ function addTag(){
 
 
 $('#doSearch').click(function () {
+
     let url = $('#info').val();
     let option = $('#option').val();
     let attr = $('#current').html();
+
     switch (option) {
         case '0': return false;
-        case '1': $.post(
-            domain + "/script/run",
-            {
-                attr: attr,
-                url: url
-            }
-        ).done(function () {
-            alert('success');
-        }).fail(function () {
-            alert('fail');
-        });
-        //case '2': $.post();
+        case '1':
+
+            $.post(
+                domain + "/script/run",
+                {
+                    attr: attr,
+                    url: url
+                }
+            ).done(function () {
+                alert('success');
+            }).fail(function () {
+                alert('fail');
+            });
+        //
+        // case '2': $.post();
     }
 });
