8f47c3567234c93527586e6d823a2e86e0564688
WangKangPan <wangkangpan@cndatacom.com>
2022-04-15 18:13:48 +0800

    2022年4月11日 补充comments.html页面

--- user/src/main/resources/static/js/comments.js
+++ user/src/main/resources/static/js/comments.js
@@ -21,10 +21,33 @@ function setAnnouncementItemsByJson(element, items){
             items[index].c));
     }
 }
+
 //数据库需要order by data desc
 $(document).ready(function (){
     let testData =  [{"a":"xiao","b":12,"c":"2022-4-31"},
+        {"a":"la","b":15,"c":"2022-4-14"},{"a":"la","b":15,"c":"2022-4-14"},
+        {"a":"la","b":15,"c":"2022-4-14"},{"a":"la","b":15,"c":"2022-4-14"},
+        {"a":"la","b":15,"c":"2022-4-14"},{"a":"la","b":15,"c":"2022-4-14"},
+        {"a":"la","b":15,"c":"2022-4-14"},{"a":"la","b":15,"c":"2022-4-14"},
         {"a":"la","b":15,"c":"2022-4-14"}];
     let element = $('#announcement-father');
+    $('#pageLimit').bootstrapPaginator({
+        currentPage: 1,
+        totalPages: 10,
+        size:"normal",
+        bootstrapMajorVersion: 3,
+        alignment:"right",
+        numberOfPages:5,
+        itemTexts: function (type, page, current) {
+            switch (type) {
+                case "first": return "首页";
+                case "prev": return "上一页";
+                case "next": return "下一页";
+                case "last": return "末页";
+                case "page": return page;
+            }
+        }
+    });
     setAnnouncementItemsByJson(element, testData);
 })
+
